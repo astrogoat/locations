5b495133a6fbe071c68cca2dd13d50687db81286
Peter <oniicoder@gmail.com>
2022-01-04 10:46:11 +0000

    Add api key to settings that should be saved for locations app

--- database/migrations/settings/2021_09_30_100000_create_locations_settings.php
+++ database/migrations/settings/2021_09_30_100000_create_locations_settings.php
@@ -7,14 +7,14 @@ class CreateLocationsSettings extends SettingsMigration
     public function up(): void
     {
         $this->migrator->add('locations.enabled', false);
-        // $this->migrator->add('locations.url', '');
+         $this->migrator->add('locations.api_key', '');
         // $this->migrator->addEncrypted('locations.access_token', '');
     }
 
     public function down()
     {
         $this->migrator->delete('locations.enabled');
-        // $this->migrator->delete('locations.url');
+         $this->migrator->delete('locations.api_key');
         // $this->migrator->delete('locations.access_token');
     }
 }
