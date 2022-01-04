e19cb7ab081cb691df4bc07a8cf5906de29af599
Peter <oniicoder@gmail.com>
2022-01-04 10:54:32 +0000

    Specify required inputs, fetch footers

--- src/Http/Livewire/Models/LocationForm.php
+++ src/Http/Livewire/Models/LocationForm.php
@@ -4,7 +4,10 @@ namespace Astrogoat\Locations\Http\Livewire\Models;
 
 use Astrogoat\Locations\Models\Location;
 use Helix\Lego\Http\Livewire\Models\Form;
+use Helix\Lego\Models\Footer;
 use Helix\Lego\Models\Model;
+use Helix\Lego\Rules\SlugRule;
+use Illuminate\Support\Str;
 
 class LocationForm extends Form
 {
@@ -14,12 +17,16 @@ class LocationForm extends Form
     {
         return [
             'location.store_name' => 'required',
+            'location.slug' => [new SlugRule($this->location)],
             'location.partner_name' => 'required',
             'location.store_address' => 'required',
             'location.store_contact_phone_number' => 'required',
             'location.store_display_phone_number' => 'required',
-            'location.store_page_url' => 'required',
             'location.store_hours' => 'required',
+            'location.lat' => 'required',
+            'location.lng' => 'required',
+            'location.layout' => 'nullable',
+            'location.footer_id' => 'nullable',
         ];
     }
 
@@ -32,6 +39,24 @@ class LocationForm extends Form
     {
         if ($this->location->wasRecentlyCreated) {
             return redirect()->to(route('lego.locations.edit', $this->location));
+        }
+    }
+
+    public function updating($property, $value)
+    {
+        parent::updating($property, $value);
+
+        if ($property == 'location.store_name' && ! $this->location->exists) {
+            $this->location->slug = Str::slug($value);
+        }
+    }
+
+    public function updated($property, $value)
+    {
+        parent::updated($property, $value);
+
+        if ($property == 'location.footer_id' && ! $value) {
+            $this->location->footer_id = null;
         }
     }
 
@@ -53,5 +78,16 @@ class LocationForm extends Form
     public function getModel(): Model
     {
         return $this->location;
+    }
+
+    public function footers()
+    {
+        return Footer::all()->pluck('title', 'id');
+    }
+
+    public function setLatLng($address, $lat, $lng) {
+        $this->location->store_address = $address;
+        $this->location->lat = $lat;
+        $this->location->lng = $lng;
     }
 }
