f29c26a071f59841cc8ddbae094133b27a541458
Peter <oniicoder@gmail.com>
2022-01-04 10:44:27 +0000

    Make changes to locations table

--- database/migrations/create_locations_table.php
+++ database/migrations/create_locations_table.php
@@ -8,21 +8,27 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('locations_table', function (Blueprint $table) {
+        Schema::create('locations', function (Blueprint $table) {
             $table->increments('id');
             $table->string('store_name');
             $table->string('partner_name');
             $table->string('store_address');
             $table->string('store_display_phone_number');
             $table->string('store_contact_phone_number');
-            $table->string('store_page_url');
             $table->string('store_hours');
+            $table->string('slug');
+            $table->double('lat');
+            $table->double('lng');
+            $table->string('layout')->nullable();
+            $table->unsignedInteger('footer_id')->nullable();
             $table->timestamps();
+
+            $table->foreign('footer_id')->references('id')->on('footers');
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('locations_table');
+        Schema::dropIfExists('locations');
     }
 };
