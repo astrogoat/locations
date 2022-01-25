1409a9c16f7529d3d057335244b7e2e563d42b0c
Peter <oniicoder@gmail.com>
2022-01-25 21:20:11 +0100

    Update db column names

--- database/migrations/create_locations_table.php
+++ database/migrations/create_locations_table.php
@@ -10,15 +10,15 @@ return new class extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('store_name');
-            $table->string('partner_name');
-            $table->string('store_address');
-            $table->string('store_display_phone_number');
-            $table->string('store_contact_phone_number');
-            $table->string('store_hours');
+            $table->string('name');
+            $table->string('address');
+            $table->string('display_phone_number');
+            $table->string('contact_phone_number');
+            $table->text('open_hours');
             $table->string('slug');
             $table->double('lat');
             $table->double('lng');
+            $table->string('place_id');
             $table->string('layout')->nullable();
             $table->unsignedInteger('footer_id')->nullable();
             $table->timestamps();
