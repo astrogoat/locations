9d5ba8099941e356ee84aa9323b663fde59b5992
Kristoffer Tonning <tonning@gmail.com>
2022-07-12 13:48:46 +0200

    Final fix of migrations?

--- database/migrations/2022_07_10_000000_create_locations_table.php
+++ database/migrations/2022_07_10_000000_create_locations_table.php
@@ -8,6 +8,10 @@ return new class extends Migration
 {
     public function up()
     {
+        if (Schema::hasTable('locations')) {
+            return;
+        }
+
         Schema::create('locations', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
--- database/migrations/2022_07_11_000000_update_locations_table.php
+++ database/migrations/2022_07_11_000000_update_locations_table.php
@@ -10,32 +10,25 @@ return new class extends Migration
     public function up()
     {
         Schema::table('locations', function (Blueprint $table) {
-            $table->boolean('indexable');
-            $table->timestamp('published_at')->nullable();
+            // Add columns
+            $table->boolean('indexable')->after('layout');
+            $table->text('open_hours')->after('place_id')->nullable();
+            $table->timestamp('published_at')->after('footer_id')->nullable();
 
+            // Change to make nullable
             $table->string('address')->change()->nullable();
             $table->string('display_phone_number')->change()->nullable();
             $table->string('contact_phone_number')->change()->nullable();
             $table->string('place_id')->change()->nullable();
         });
 
+        // Change to make nullable
         DB::statement('ALTER TABLE locations modify `lat` DOUBLE NULL');
         DB::statement('ALTER TABLE locations modify `lng` DOUBLE NULL');
     }
 
     public function down()
     {
-        Schema::table('locations', function (Blueprint $table) {
-            $table->dropColumn('indexable');
-            $table->dropColumn('published_at');
-
-            $table->string('address')->change()->nullable(false);
-            $table->string('display_phone_number')->change()->nullable(false);
-            $table->string('contact_phone_number')->change()->nullable(false);
-            $table->string('place_id')->change()->nullable(false);
-        });
-
-        DB::statement('ALTER TABLE locations modify `lat` DOUBLE');
-        DB::statement('ALTER TABLE locations modify `lng` DOUBLE');
+        //
     }
 };
