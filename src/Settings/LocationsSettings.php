41853cecd68339c6e1c8986831f92da7781838e4
Laura Tonning <tonning@gmail.com>
2024-03-24 21:22:22 +0100

    Clean up

--- src/Settings/LocationsSettings.php
+++ src/Settings/LocationsSettings.php
@@ -8,21 +8,11 @@ use Helix\Lego\Settings\AppSettings;
 class LocationsSettings extends AppSettings
 {
     public string $api_key;
-    // public string $access_token;
 
     protected array $rules = [
          'api_key' => ['required'],
-        // 'access_token' => ['required'],
     ];
 
-    protected static array $actions = [
-        // LocationsAction::class,
-    ];
-
-    // public static function encrypted(): array
-    // {
-    //     return ['access_token'];
-    // }
 
     public function description(): string
     {
