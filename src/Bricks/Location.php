eda0136a16cac468e0b659da970aeedcf183256a
Laura Tonning <tonning@gmail.com>
2025-08-25 15:19:24 +0200

    DTCE-2943: Add functionality for bricks to have separate content for variations (#12)

--- src/Bricks/Location.php
+++ src/Bricks/Location.php
@@ -6,11 +6,6 @@ use Helix\Lego\Bricks\Brick;
 
 class Location extends Brick
 {
-    public function getDefaults()
-    {
-        return $this->default;
-    }
-
     public function brickView(): string
     {
         return 'locations::bricks.location';
